a2431cb10c75d4317edb1e4d6fb9d1c37c77f735
rabinabdian <rabin.abdian@gmail.com>
2021-01-05 19:24:13 +0200

    select all

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,14 +16,27 @@ public class Main {
 
             Connection conn = DriverManager.getConnection(dbURL, username, password);
             PreparedStatement ps=null;
+            ResultSet rs = null;
 
             if (conn != null) {
                 System.out.println("Connected");
-                String query = "insert into users(user_id, username,password,fullname,email) values ('2','AAA','0000','ASSESSES','a@a.com')";
-                ps=conn.prepareStatement(query);
+                String queryInsert = "insert into users(user_id, username,password,fullname,email) values ('5','AAA','0000','ASSESSES','a@a.com')";
+                String querySelectAll = "select * from users";
+
+                ps=conn.prepareStatement(queryInsert);
                 ps.executeUpdate();
                 System.out.println("Inserted");
+                ps=conn.prepareStatement(querySelectAll);
+                rs = ps.executeQuery();
 
+                ResultSetMetaData metaData = rs.getMetaData();
+                int columnCount = metaData.getColumnCount();
+                while(rs.next()) {
+                    for(int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                        Object object = rs.getObject(columnIndex);
+                        System.out.printf("%s, ", object == null ? "NULL" : object.toString());
+                    }
+                    System.out.printf("%n");}
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
