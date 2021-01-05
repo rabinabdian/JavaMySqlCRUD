3052732f968c5a18c1108f43f3c2b6e4f00cbc7e
rabinabdian <rabin.abdian@gmail.com>
2021-01-05 22:29:28 +0200

    queryDeleteByUserID

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import java.sql.*;
 
 import static java.lang.Math.random;
+import static java.lang.Math.round;
 
 public class Main {
 
@@ -22,13 +23,16 @@ public class Main {
 
             if (conn != null) {
                 System.out.println("Connected");
-                String queryInsert = "insert into users(user_id, username,password,fullname,email) values ('8','AAA','0000','ASSESSES','a@a.com')";
+                String queryInsert1 = "insert into users(user_id, username,password,fullname,email) values (1200,'AAA','0000','ASSESSES','a@a.com'),(11500,'AAA','0000','ASSESSES','a@a.com')";
                 String querySelectAll = "select * from users";
                 String querySelectByUserID = "select * from users where user_id = 6";
-                String queryUpdateByUserID = "update users set user_id = "+random() * (100 - 20 + 1)+"where user_id = 8";
+
+                String queryUpdateByUserID = "update users set user_id = "+random() * (1000 - 20 + 1)+"where user_id = 1200";
+                String queryDeleteByUserID = "delete from users where user_id = 11500"; //
+
                 ps=conn.prepareStatement(queryUpdateByUserID);
                 ps.executeUpdate();
-                ps=conn.prepareStatement(queryInsert);
+                ps=conn.prepareStatement(queryInsert1);
                 ps.executeUpdate();
                 System.out.println("Inserted");
                 ps=conn.prepareStatement(querySelectAll);
@@ -37,6 +41,13 @@ public class Main {
                 ps=conn.prepareStatement(querySelectByUserID);
                 rs = ps.executeQuery();
                 printSelectAll(rs);
+                System.out.println("============ after delete ===========");
+                ps=conn.prepareStatement(queryDeleteByUserID);
+                ps.executeUpdate();
+                ps=conn.prepareStatement(querySelectAll);
+                rs = ps.executeQuery();
+                printSelectAll(rs);
+
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
@@ -49,7 +60,7 @@ public class Main {
 
         int columnCount = metaData.getColumnCount();
         System.out.println("****************************************************");
-        while(rs.next()) {
+        while(rs.next() && rs!=null) {
             for(int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
                 Object object = rs.getObject(columnIndex);
                 System.out.printf("%s, ", object == null ? "NULL" : object.toString());
